a25d206d1d44af391074c4400f1a9ceb3c9910f7
Cecilia CR <76215676+CecilCR@users.noreply.github.com>
2026-07-18 21:41:53 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-alert("SPAE MVP cargado correctamente.");
+document.addEventListener("DOMContentLoaded", () => {
+
+    const content = document.getElementById("content");
+
+    content.innerHTML = `
+        <h2>SPAE MVP operativo</h2>
+
+        <button onclick="saludar()">
+            Probar botón
+        </button>
+    `;
+
+});
+
+
+function saludar(){
+
+    alert("El botón funciona correctamente.");
+
+}
